91809e292a5016786fb70e08a715ae5af8d41557
amr-ai2004 <amraimahadeen@gmail.com>
2025-05-13 21:06:13 +0300

    Did a bit of restyling to further match the general look of the page examples givien to us in the 'In-Class Assignment 4'.

--- movieExplorer/src/components/MyNavbar.jsx
+++ movieExplorer/src/components/MyNavbar.jsx
@@ -7,7 +7,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 export default function MyNavbar({movies}) {
   return (
     <nav>
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
           <Container>
             <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -15,7 +15,7 @@ export default function MyNavbar({movies}) {
               <Nav className="me-auto">
                 <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="/movies">List of movies</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown title="Movie Details" id="basic-nav-dropdown">
                 {movies != undefined ? 
                       movies.map(item=>
                       (<NavDropdown.Item href={`/moviedetails/${item.id}`}>{item.title}</NavDropdown.Item>))
